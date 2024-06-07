694f41469ddc10c732010fc4328a2fe9f4683c2a
hugoAssal <hugo.assal1@etu.univ-lorraine.fr>
2024-06-07 11:17:42 +0200

    interface collection en array

--- src/core/services/ServiceCatalogueInterface.php
+++ src/core/services/ServiceCatalogueInterface.php
@@ -8,7 +8,7 @@ interface ServiceCatalogueInterface
 {
     public function getCategories(): array;
     public function getCategorieById(int $id): array;
-    public function getPrestations(): Collection;
+    public function getPrestations(): array;
     public function getPrestationById(string $id): array;
     public function getPrestationsbyCategorie(int $categ_id): array;
     public function createCategorie(array $tabCateg): int;
